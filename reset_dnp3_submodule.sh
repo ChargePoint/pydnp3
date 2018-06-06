54545a3853e1787e27175247e8e627303fe4aa21
Anh <anhhng141@gmail.com>
2018-06-06 12:18:28 -0700

    Fixed #1 - Change dnp3 git submodule to automatak/dnp3 commit 71e767f - Add DNP3Manager destructor with gil_scope_release - Update reset_dnp3_submodule.sh to checkout commit 71e767f - Edit test after solving the hang problem

--- reset_dnp3_submodule.sh
+++ reset_dnp3_submodule.sh
@@ -2,6 +2,8 @@ git rm --cached deps/dnp3
 rm -rf .git/modules/deps/dnp3
 rm -rf deps/dnp3
 cd deps
-git submodule add https://github.com/Kisensum/dnp3.git
+git submodule add https://github.com/automatak/dnp3.git
 git submodule update --init --recursive
-cd ..
+cd dnp3
+git checkout 71e767f4c4baae8e866406b964d9e8ffaafa02d8
+cd ../..
