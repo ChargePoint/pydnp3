48cd2e89284017a72c0e0871bc57ab73fd356f4f
Rory Yorke <rory.yorke@ario.co.za>
2021-12-09 10:57:03 +0200

    Added extra pybind11 headers in src/pydnp3opendnp3_?.hpp
    From a very helpful error message from pybind11.

--- src/pydnp3opendnp3_2.hpp
+++ src/pydnp3opendnp3_2.hpp
@@ -1,6 +1,11 @@
 #ifndef PYDNP3OPENDNP3_2_HPP
 #define PYDNP3OPENDNP3_2_HPP
 
+#include <pybind11/stl.h>
+#include <pybind11/complex.h>
+#include <pybind11/functional.h>
+#include <pybind11/chrono.h>
+
 #include <pybind11/pybind11.h>
 
 void init_opendnp3_2(pybind11::module &opendnp3);
